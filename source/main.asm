c391a081b8ec279c958414237480a4f74c5e4c2d
MobSlicer152 <brambleclaw1414@gmail.com>
2023-10-11 17:42:17 -0400

    Add nasmx and start getting code to compile again

--- source/main.asm
+++ source/main.asm
@@ -1,19 +1,23 @@
-	bits 64
+        bits 64
 
-	%include "nt.inc"
-	%include "pong.inc"
-	%include "window.inc"
+        %include "nasmx.inc"
+        %include "win32/kernel32.inc"
+        %include "win32/windows.inc"
+        %include "pong.inc"
+        %include "window.inc"
 
-	section .text
-	global mainCRTStartup
+        extern ExitProcess
+
+        section .text
+        global mainCRTStartup
 mainCRTStartup:
-	push rbp
-	mov rbp, rsp
+        push rbp
+        mov rbp, rsp
 
-	sub rsp, 32
-	call SetupWindow
-	add rsp, 32
+        sub rsp, 32
+        call SetupWindow
+        add rsp, 32
 
-	mov rcx, NtCurrentProcess()
-	mov edx, STATUS_SUCCESS
-	call NtTerminateProcess
+        sub rsp, 32
+        mov ecx, 0                     ; No error
+        call ExitProcess
--- source/window.asm
+++ source/window.asm
@@ -1,104 +1,110 @@
-	bits 64
+        bits 64
 
-	%include "nt.inc"
-	%include "pong.inc"
+        %include "nasmx.inc"
+        %include "win32/kernel32.inc"
+        %include "win32/user32.inc"
+        %include "pong.inc"
 
-	section .text
+        extern RegisterClassExW
+        extern CreateWindowExW
+        extern ExitProcess
+        extern ShowWindow
 
-	global SetupWindow
+        section .text
+
+        global SetupWindow
 SetupWindow:
-								; Stack setup
-	push rbp
-	mov rbp, rsp
+; Stack setup
+        push rbp
+        mov rbp, rsp
 
-	push r12
-	push r13
+        push r12
+        push r13
 
-								; Variables
-	sub rsp, sizeof(WNDCLASSEX)
+; Variables
+        sub rsp, sizeof(WNDCLASSEXW)
 
-								; Clear WNDCLASSEX
-	cld
-	mov r12, rsp
-	mov rdi, r12
-	xor eax, eax
-	mov ecx, sizeof(WNDCLASSEX)
-	rep stosb
+; Clear WNDCLASSEXW
+        cld
+        mov r12, rsp
+        mov rdi, r12
+        xor eax, eax
+        mov ecx, sizeof(WNDCLASSEXW)
+        rep stosb
 
-	mov r13, sizeof(WNDCLASSEX)				; sizeof(WNDCLASSEX) -> cbSize
-	mov dword [r12 + WNDCLASSEX.cbSize], r13d
-	lea r13, [rel WindowProcedure]				; WindowProcedure -> lpfnWndProc
-	mov qword [r12 + WNDCLASSEX.lpfnWndProc], r13
-	lea r13, [rel className]				; className -> lpszClassName
-	mov qword [r12 + WNDCLASSEX.lpszClassName], r13
+        mov r13, sizeof(WNDCLASSEXW)    ; sizeof(WNDCLASSEXW) -> cbSize
+        mov dword [r12 + WNDCLASSEXW.cbSize], r13d
+        lea r13, [rel WindowProcedure] ; WindowProcedure -> lpfnWndProc
+        mov qword [r12 + WNDCLASSEXW.lpfnWndProc], r13
+        lea r13, [rel className]       ; className -> lpszClassName
+        mov qword [r12 + WNDCLASSEXW.lpszClassName], r13
 
-								; RegisterClassExW
-	sub rsp, 32						; Shadow space
-	mov rcx, r12						; Window class structure
-	call RegisterClassExW
-	add rsp, 32						; Clear shadow space
+; RegisterClassExW
+        sub rsp, 32                    ; Shadow space
+        mov rcx, r12                   ; Window class structure
+        call RegisterClassExW
+        add rsp, 32                    ; Clear shadow space
 
-								; CreateWindowExW
-	sub rsp, 32 + 4 + 4 + 4 + 4 + 8 + 8 + 8 + 8
-	xor ecx, ecx						; No extended styles
-	mov rdx, qword [r12 + WNDCLASSEX.lpszClassName]		; Class name
-	lea r8, [rel windowName]				; Window name
-	mov r9, WS_OVERLAPPEDWINDOW				; Generic window style
-	mov r13, CW_USEDEFAULT					; CW_USEDEFAULT for X, Y, nWidth, nHeight
-	mov dword [rsp + 44], r13d
-	mov dword [rsp + 40], r13d
-	mov dword [rsp + 36], r13d
-	mov dword [rsp + 32], r13d
-	mov qword [rsp + 24], rcx				; RCX is already zeroed, and rest of parameters are NULL
-	mov qword [rsp + 16], rcx
-	mov qword [rsp + 8], rcx
-	mov qword [rsp + 0], rcx
-	call CreateWindowExW
-	add rsp 32
+; CreateWindowExW
+        sub rsp, 32 + 4 + 4 + 4 + 4 + 8 + 8 + 8 + 8
+        xor ecx, ecx                   ; No extended styles
+        mov rdx, qword [r12 + WNDCLASSEXW.lpszClassName] ; Class name
+        lea r8, [rel windowName]       ; Window name
+        mov r9, WS_OVERLAPPEDWINDOW    ; Generic window style
+        mov r13, CW_USEDEFAULT         ; CW_USEDEFAULT for X, Y, nWidth, nHeight
+        mov dword [rsp + 44], r13d
+        mov dword [rsp + 40], r13d
+        mov dword [rsp + 36], r13d
+        mov dword [rsp + 32], r13d
+        mov qword [rsp + 24], rcx      ; RCX is already zeroed, and rest of parameters are NULL
+        mov qword [rsp + 16], rcx
+        mov qword [rsp + 8], rcx
+        mov qword [rsp + 0], rcx
+        call CreateWindowExW
+        add rsp, 32
 
-								; Check window
-	cmp rax, 0
-	jne .success
+; Check window
+        cmp rax, 0
+        jne .success
 
 .error:
-	mov ecx, NtCurrentProcess()
-	mov edx, 1
-	call NtTerminateProcess
+        mov ecx, 1
+        call ExitProcess
 .success:
-	mov [rel window], rax
-	mov rcx, [rel window]
-	mov edx, SW_SHOWNORMAL
-	call ShowWindow
+        mov [rel window], rax
+        mov rcx, [rel window]
+        mov edx, SW_SHOWNORMAL
+        call ShowWindow
 
-								; Stack cleanup
-	add rsp, sizeof(WNDCLASSEX)
+; Stack cleanup
+        add rsp, sizeof(WNDCLASSEXW)
 
-	pop r13
-	pop r12
+        pop r13
+        pop r12
 
-	pop rbp
-	ret
+        pop rbp
+        ret
 
-	global WindowProcedure
+        global WindowProcedure
 WindowProcedure:
-	push rbp
-	mov rbp, rsp
+        push rbp
+        mov rbp, rsp
 
-	mov eax, 0
+        mov eax, 0
 
-	pop rbp
-	ret
+        pop rbp
+        ret
 
-	section .data
+        section .data
 className:
-	dw L("PongClass"), 0
+        dw L("PongClass"), 0
 .size:
-	dq $ - className
+        dq $ - className
 windowName:
-	dw L("Pong"), 0
+        dw L("Pong"), 0
 .size:
-	dq $ - windowName
+        dq $ - windowName
 
-	section .bss
+        section .bss
 window:
-	resq 1
+        resq 1
